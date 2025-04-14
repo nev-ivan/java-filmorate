918560b4641d73b07d6b1142f3d34c8278747363
Ivan Nevostruev <Nevostruev27is@ya.ru>
2025-04-14 22:21:25 +0300

    Вторая итерация ТЗ10

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
